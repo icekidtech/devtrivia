507eb651831dda78fce1527c1c4e1ca513fda67f
icekidtech <princekelvin7568@gmail.com>
2025-04-18 02:21:06 +0100

    Add QuestionController and QuestionService to AppModule for quiz question management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,12 @@ import { PrismaService } from './prisma/prisma.service';
 import { QuizController } from './quizzes/quiz.controller';
 import { QuizService } from './quizzes/quiz.service';
 import { UserController } from './users/user.controller';
+import { QuestionController } from './questions/question.controller';
+import { QuestionService } from './questions/question.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, QuizController, UserController],
-  providers: [AppService, PrismaService, QuizService],
+  controllers: [AppController, QuizController, UserController, QuestionController],
+  providers: [AppService, PrismaService, QuizService, QuestionService],
 })
 export class AppModule {}
