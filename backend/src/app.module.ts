4d9cd83b618f5550e6e5d6041b826158c8c7f6af
icekidtech <princekelvin7568@gmail.com>
2025-04-18 01:55:59 +0100

    Add UserController to AppModule for user management functionality

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { QuizController } from './quizzes/quiz.controller';
 import { QuizService } from './quizzes/quiz.service';
+import { UserController } from './users/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, QuizController],
+  controllers: [AppController, QuizController, UserController],
   providers: [AppService, PrismaService, QuizService],
 })
 export class AppModule {}
